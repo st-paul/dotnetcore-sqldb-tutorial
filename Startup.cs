c68a7cc4178b535d9978cf7fa19660e662140189
Pavel Statkevich <pavel_statkevich@epam.com>
2021-11-18 13:50:07 +0300

    fix: connect to db

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,16 @@ namespace DotNetCoreSqlDb
             // Add framework services.
             services.AddMvc();
 
-            services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<MyDatabaseContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
+            } 
+            else
+            {
+                services.AddDbContext<MyDatabaseContext>(options =>
+                        options.UseSqlite("Data Source=MvcMove.db"));
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
